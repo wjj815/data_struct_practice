799b5133fd5c9619957f3f88618d8daecddf6404
wangjj <1090086767@qq.com>
2022-03-13 12:08:39 +0800

    单调栈：去除重复字母， 321拼接最大数（看不懂）

--- src/main/java/org/example/demo/leetcode/stack/monotonous/Solution.java
+++ src/main/java/org/example/demo/leetcode/stack/monotonous/Solution.java
@@ -84,4 +84,144 @@ public class Solution {
         }
         return res;
     }
+
+
+    /**
+     * 316. 去除重复字母
+     * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+     * @param s 输入："bcabc"
+     *
+     * @return 输出："abc"
+     */
+    public String removeDuplicateLetters(String s) {
+        // 使用 StringBuilder 来维护单调栈
+        StringBuilder res = new StringBuilder();
+        // 记录字符是否已访问过
+        boolean[] via = new boolean[128];
+        // 记录每个字符出现的次数
+        int[] count = new int[128];
+
+        char[] chars = s.toCharArray();
+        // 统计每个字符的个数
+        for (char c : chars) {
+            count[c]++;
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+
+            char c = chars[i];
+            // 缩减
+            count[c]--;
+            // 如果res 中已存在该字符则跳过（去重）
+            if(via[c]) continue;
+            // 如果当前字符 c 比 res 的最后一个字符小
+            while(res.length() > 0
+                    && (res.charAt(res.length() - 1)) > c) {
+                // 如果res 的最后一个字符在后面不会出现了，则停止回删
+                if(count[res.charAt(res.length() - 1)] == 0) {
+                    break;
+                }
+                //回删 res 中的字符，直到 res 的最后一个字符比当前字符小
+                via[res.charAt(res.length() - 1)] = false;
+                res.deleteCharAt(res.length() - 1);
+
+            }
+            // 添加 c 到 res 中
+            res.append(c);
+            // 标记该字符已添加到 res 中
+            via[c] = true;
+        }
+
+        return res.toString();
+    }
+
+
+    /**
+     *
+     * 321. 拼接最大数
+     * 给定长度分别为 m 和 n 的两个数组，其元素由 0-9 构成，表示两个自然数各位上的数字。现在从这两个数组中选出 k (k <= m + n) 个数字拼接成一个新的数，要求从同一个数组中取出的数字保持其在原数组中的相对顺序。
+     *
+     * 求满足该条件的最大数。结果返回一个表示该最大数的长度为 k 的数组。
+     *
+     * 说明: 请尽可能地优化你算法的时间和空间复杂度。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/create-maximum-number
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length, n = nums2.length;
+        int[] maxSubsequence = new int[k];
+        int start = Math.max(0, k - n), end = Math.min(k, m);
+        for (int i = start; i <= end; i++) {
+            int[] subsequence1 = maxSubsequence(nums1, i);
+            int[] subsequence2 = maxSubsequence(nums2, k - i);
+            int[] curMaxSubsequence = merge(subsequence1, subsequence2);
+            if (compare(curMaxSubsequence, 0, maxSubsequence, 0) > 0) {
+                System.arraycopy(curMaxSubsequence, 0, maxSubsequence, 0, k);
+            }
+        }
+        return maxSubsequence;
+    }
+
+    public int[] maxSubsequence(int[] nums, int k) {
+        int length = nums.length;
+        int[] stack = new int[k];
+        int top = -1;
+        int remain = length - k;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            while (top >= 0 && stack[top] < num && remain > 0) {
+                top--;
+                remain--;
+            }
+            if (top < k - 1) {
+                stack[++top] = num;
+            } else {
+                remain--;
+            }
+        }
+        return stack;
+    }
+
+    public int[] merge(int[] subsequence1, int[] subsequence2) {
+        int x = subsequence1.length, y = subsequence2.length;
+        if (x == 0) {
+            return subsequence2;
+        }
+        if (y == 0) {
+            return subsequence1;
+        }
+        int mergeLength = x + y;
+        int[] merged = new int[mergeLength];
+        int index1 = 0, index2 = 0;
+        for (int i = 0; i < mergeLength; i++) {
+            if (compare(subsequence1, index1, subsequence2, index2) > 0) {
+                merged[i] = subsequence1[index1++];
+            } else {
+                merged[i] = subsequence2[index2++];
+            }
+        }
+        return merged;
+    }
+
+    public int compare(int[] subsequence1, int index1, int[] subsequence2, int index2) {
+        int x = subsequence1.length, y = subsequence2.length;
+        while (index1 < x && index2 < y) {
+            int difference = subsequence1[index1] - subsequence2[index2];
+            if (difference != 0) {
+                return difference;
+            }
+            index1++;
+            index2++;
+        }
+        return (x - index1) - (y - index2);
+    }
+
+
+
 }
