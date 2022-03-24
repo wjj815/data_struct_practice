9cf8f0108cd677d9212fea196f90bf67fa632372
wangjj <1090086767@qq.com>
2022-03-24 23:03:04 +0800

    k 个一组翻转链表

--- src/main/java/org/example/demo/leetcode/link/LinkReverser.java
+++ src/main/java/org/example/demo/leetcode/link/LinkReverser.java
@@ -85,4 +85,68 @@ public class LinkReverser {
         // 然后归到头节点
         return head;
     }
+
+
+    /**
+     * 反转区间[a,b)的元素， 注意这是左闭右开
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre = null, cur = a, next = null;
+        // while 终止条件改一下就行了
+        while(cur != b) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        // 返回反转后的头结点
+        return pre;
+    }
+
+    /**
+     *
+     * 25. K 个一组翻转链表
+     * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+     *
+     * k 是一个正整数，它的值小于或等于链表的长度。
+     *
+     * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+     *
+     * 进阶：
+     *
+     * 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+     * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+     *  
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null) return null;
+        // 区间[a, b) 包含 k 个待反转的元素
+        ListNode a, b;
+        a = b = head;
+        for(int i = 0; i < k; i++) {
+            // 不足 k 个， 不需要反转， base case
+            if(b == null) return head;
+            b = b.next;
+        }
+
+        // 反转前 k 个元素
+        ListNode newHead = reverse(a, b);
+        // 递归反转后续链表并连接起来
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
+
+
+
 }
