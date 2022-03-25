f447a9f5850479bcf44dd8294d3dc041a649e9e9
wangjj <1090086767@qq.com>
2022-03-26 00:04:25 +0800

    根据前序遍历序列化以及反序列化

--- src/main/java/org/example/demo/leetcode/tree/codec/Codec.java
+++ src/main/java/org/example/demo/leetcode/tree/codec/Codec.java
@@ -1,2 +1,20 @@
-package org.example.demo.leetcode.tree.codec;public interface Codec {
+package org.example.demo.leetcode.tree.codec;
+
+import org.example.demo.tree.TreeNode;
+
+public abstract class Codec {
+
+    /**
+     * 把一颗二叉树序列化成字符串
+     * @param root
+     * @return
+     */
+    public abstract String serialize(TreeNode root);
+
+    /**
+     * 把字符串反序列化成二叉树
+     * @param data
+     * @return
+     */
+    public abstract TreeNode deserialize(String data);
 }
--- src/main/java/org/example/demo/leetcode/tree/codec/PreOrderCodec.java
+++ src/main/java/org/example/demo/leetcode/tree/codec/PreOrderCodec.java
@@ -1,2 +1,61 @@
-package org.example.demo.leetcode.tree.codec;public class PreOrderCodec {
+package org.example.demo.leetcode.tree.codec;
+
+import org.example.demo.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PreOrderCodec extends Codec{
+
+    // 代表分割符
+    String SEP = ",";
+    // 代表 null 空指针字符
+    String NULL = "#";
+    // 用于拼接字符串
+
+
+    @Override
+    public String serialize(TreeNode root) {
+        List<String> strings = new ArrayList<>();
+        traverse(root, strings);
+        return String.join(SEP, strings);
+    }
+
+    public void traverse(TreeNode root,List<String> strings) {
+        if(root == null) {
+            strings.add(NULL);
+            return;
+        }
+
+        // 前序遍历
+        strings.add(String.valueOf(root.val));
+
+        traverse(root.left, strings);
+        traverse(root.right, strings);
+    }
+
+    @Override
+    public TreeNode deserialize(String data) {
+        LinkedList<String> nodes = new LinkedList<>(Arrays.asList(data.split(SEP)));
+
+        return deserialize(nodes);
+    }
+
+    public TreeNode deserialize(LinkedList<String> nodes) {
+        if(nodes.isEmpty()) return null;
+
+        // 前序遍历位置
+        // 列表最左侧就是根节点
+        String first = nodes.removeFirst();
+
+        if(first.equals(NULL)) return null;
+
+        TreeNode root = new TreeNode(Integer.parseInt(first));
+
+        root.left = deserialize(nodes);
+        root.right = deserialize(nodes);
+        return root;
+    }
 }
