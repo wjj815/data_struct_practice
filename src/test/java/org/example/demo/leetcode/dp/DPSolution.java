ec13d30ed42afd883b542d1194e42facfe4fceac
wangjj <1090086767@qq.com>
2022-04-25 23:02:34 +0800

    动态规划: 自由之路

--- src/test/java/org/example/demo/leetcode/dp/DPSolution.java
+++ src/test/java/org/example/demo/leetcode/dp/DPSolution.java
@@ -1,9 +1,12 @@
 package org.example.demo.leetcode.dp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DPSolution {
-
 
     /**
      * 518. 零钱兑换 II
@@ -35,7 +38,6 @@ public class DPSolution {
         return dp[amount];
     }
 
-
     /**
      * 931. 下降路径最小和
      * 难度
@@ -43,7 +45,10 @@ public class DPSolution {
      * <p>
      * 给你一个 n x n 的 方形 整数数组 matrix ，请你找出并返回通过 matrix 的下降路径 的 最小和 。
      * <p>
-     * 下降路径 可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列（即位于正下方或者沿对角线向左或者向右的第一个元素）。具体来说，位置 (row, col) 的下一个元素应当是 (row + 1, col - 1)、(row + 1, col) 或者 (row + 1, col + 1) 。
+     * 下降路径
+     * 可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列（即位于正下方或者沿对角线向左或者向右的第一个元素）。具体来说，位置
+     * (row, col) 的下一个元素应当是 (row + 1, col - 1)、(row + 1, col) 或者 (row + 1, col + 1)
+     * 。
      * <p>
      * 输入：matrix = [[2,1,3],[6,5,4],[7,8,9]]
      * 输出：13
@@ -84,8 +89,7 @@ public class DPSolution {
                         dp(matrix, i - 1, j - 1),
                         Math.min(
                                 dp(matrix, i - 1, j),
-                                dp(matrix, i - 1, j + 1))
-                );
+                                dp(matrix, i - 1, j + 1)));
             }
         }
         int m = matrix.length;
@@ -135,4 +139,141 @@ public class DPSolution {
 
         return res;
     }
+
+    /**
+     * 514. 自由之路
+     * 电子游戏“辐射4”中，任务 “通向自由” 要求玩家到达名为 “Freedom Trail Ring” 的金属表盘，并使用表盘拼写特定关键词才能开门。
+     * 
+     * 给定一个字符串 ring ，表示刻在外环上的编码；给定另一个字符串 key ，表示需要拼写的关键词。您需要算出能够拼写关键词中所有字符的最少步数。
+     * 
+     * 最初，ring 的第一个字符与 12:00 方向对齐。您需要顺时针或逆时针旋转 ring 以使 key 的一个字符在 12:00
+     * 方向对齐，然后按下中心按钮，以此逐个拼写完 key 中的所有字符。
+     * 
+     * 旋转 ring 拼出 key 字符 key[i] 的阶段中：
+     * 
+     * 您可以将 ring 顺时针或逆时针旋转 一个位置 ，计为1步。旋转的最终目的是将字符串 ring 的一个字符与 12:00
+     * 方向对齐，并且这个字符必须等于字符 key[i] 。
+     * 如果字符 key[i] 已经对齐到12:00方向，您需要按下中心按钮进行拼写，这也将算作 1
+     * 步。按完之后，您可以开始拼写 key 的下一个字符（下一阶段）, 直至完成所有拼写。
+     * 
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/freedom-trail
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * 
+     * @param ring
+     * @param key
+     * @return
+     */
+    public int findRotateSteps(String ring, String key) {
+
+        Map<Character, List<Integer>> map = new HashMap<>();
+
+        class DP {
+            // 字符 -> 索引列表
+            
+            // 备忘录
+            final int[][] memo;
+            public DP(int[][] memo) {
+                this.memo = memo;
+            }
+
+            // 计算圆盘指针在 ring[i],输入 key[j...] 的最少操作数
+            public int dp(String ring, int i, String key, int j) {
+                // base case 
+                if(j == key.length()) {
+                    return 0;
+                }
+                // 查找备忘录，避免重叠子问题
+                if(memo[i][j] != 0) {
+                    return memo[i][j];
+                }
+
+                int n =  ring.length();
+                // 做选择
+                int res = Integer.MAX_VALUE;
+                // ring 上可能有多个字符 key[j]
+                for(int k : map.get(key.charAt(j))) {
+                    // 拨动指针的次数
+                    int delta = Math.abs(k - i);
+                    // 选择顺时针还是逆时针
+                    delta = Math.min(delta, n - delta);
+                    // 将指针拨到 ring[k], 继续输入 key[j + 1..]
+                    int subProblem = dp(ring, k, key, j + 1);
+                    // 选择【整体】操作次数最少的
+                    res = Math.min(res, 1 + delta + subProblem);
+                    // PS: 加一是因为拨动按钮也是一次操作
+                }
+                // 将结果存入备忘录           
+                return memo[i][j] = res;        
+            }
+        }
+
+        int[][] memo = new int[ring.length()][key.length()];
+    
+        DP dp = new DP(memo);
+        for (int i = 0; i < ring.length(); i++) {
+            map.putIfAbsent(ring.charAt(i), new ArrayList<>());
+            map.get(ring.charAt(i)).add(i);
+        }
+        // 圆盘指针最初指向 12 点钟方向
+        // 从第一个字符开始输入 key
+        return dp.dp(ring, 0, key, 0);
+    }
+
+    public int findRotateStepsByDP(String ring, String key) {
+
+        Map<Character, List<Integer>> map = new HashMap<>();
+
+        class DP {
+            // 字符 -> 索引列表
+            
+            // 备忘录
+            final int[][] memo;
+            public DP(int[][] memo) {
+                this.memo = memo;
+            }
+
+            // 计算圆盘指针在 ring[i],输入 key[j...] 的最少操作数
+            public int dp(String ring, int i, String key, int j) {
+                // base case 
+                if(j == key.length()) {
+                    return 0;
+                }
+                // 查找备忘录，避免重叠子问题
+                if(memo[i][j] != 0) {
+                    return memo[i][j];
+                }
+
+                int n =  ring.length();
+                // 做选择
+                int res = Integer.MAX_VALUE;
+                // ring 上可能有多个字符 key[j]
+                for(int k : map.get(key.charAt(j))) {
+                    // 拨动指针的次数
+                    int delta = Math.abs(k - i);
+                    // 选择顺时针还是逆时针
+                    delta = Math.min(delta, n - delta);
+                    // 将指针拨到 ring[k], 继续输入 key[j + 1..]
+                    int subProblem = dp(ring, k, key, j + 1);
+                    // 选择【整体】操作次数最少的
+                    res = Math.min(res, 1 + delta + subProblem);
+                    // PS: 加一是因为拨动按钮也是一次操作
+                }
+                // 将结果存入备忘录           
+                return memo[i][j] = res;        
+            }
+        }
+
+        int[][] memo = new int[ring.length()][key.length()];
+    
+        DP dp = new DP(memo);
+        for (int i = 0; i < ring.length(); i++) {
+            map.putIfAbsent(ring.charAt(i), new ArrayList<>());
+            map.get(ring.charAt(i)).add(i);
+        }
+        // 圆盘指针最初指向 12 点钟方向
+        // 从第一个字符开始输入 key
+        return dp.dp(ring, 0, key, 0);
+    }
+
 }
